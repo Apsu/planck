97ed54e445d5387c16976db2caa5a85dae6c8844
Evan Callicoat <apsu@propter.net>
2021-04-22 02:02:13 -0500

    Add in build configs for tap options

---
+++ config.h
@@ -0,0 +1,13 @@
+/*
+ * Auto shift options
+ */
+#define AUTO_SHIFT_TIMEOUT 150
+#define AUTO_SHIFT_REPEAT
+
+/*
+ * Tap options
+ */
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
