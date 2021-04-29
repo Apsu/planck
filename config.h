eeab2db79de57a8e3389866726870c415f230aef
Evan Callicoat <apsu@propter.net>
2021-04-29 00:24:24 -0500

    Remove auto shift defines

--- config.h
+++ config.h
@@ -1,13 +1,6 @@
 /*
- * Auto shift options
- */
-#define AUTO_SHIFT_TIMEOUT 200
-#define AUTO_SHIFT_REPEAT
-
-/*
  * Tap options
  */
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
-
