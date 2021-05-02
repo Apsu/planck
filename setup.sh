c5fa2cc198f2b6baa54631c8be887c59c2b0e7a1
Evan Callicoat <apsu@propter.net>
2021-05-02 00:10:32 -0500

    Add -p to create whole path if missing

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 qmk json2c layout.json -o keymap.c
-mkdir ~/qmk_firmware/keyboards/planck/keymaps/apsu/
+mkdir -p ~/qmk_firmware/keyboards/planck/keymaps/apsu/
 cp rules.mk config.h keymap.c ~/qmk_firmware/keyboards/planck/keymaps/apsu/
 qmk flash -j 4 -kb planck/rev6 -km apsu
