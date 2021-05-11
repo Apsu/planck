88d7ed0d5dc7bb8768e6f5799fcc46c86591bb8f
Evan Callicoat <apsu@propter.net>
2021-05-11 00:59:09 -0500

    Try a wide setup

--- config.h
+++ config.h
@@ -4,7 +4,4 @@
 #define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
-// #define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
-
-#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+#define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
