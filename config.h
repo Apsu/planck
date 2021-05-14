15779dcc8a92fb58e2f2f2d06ba3db80e9bd8fe6
Evan Callicoat <apsu@propter.net>
2021-05-14 15:11:54 -0500

    Make consistent with edgemod branch

--- config.h
+++ config.h
@@ -7,4 +7,4 @@
 // #define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
 
 #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+#define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
