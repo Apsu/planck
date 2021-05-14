2c35673bdc1a7cb69534e79c0d5d9ba2cc50dc5c
Evan Callicoat <apsu@propter.net>
2021-05-14 17:41:50 -0500

    Update with edgemods

--- config.h
+++ config.h
@@ -4,3 +4,7 @@
 #define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
+// #define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
+
+#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TIMEOUT 2000
