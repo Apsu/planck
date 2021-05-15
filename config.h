5c06dd9667c2c3076917e8fe1c703c9edfc270f4
Evan Callicoat <apsu@propter.net>
2021-05-15 14:40:34 -0500

    Revert tap options to stock

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
  * Tap options
  */
 #define TAPPING_TERM 200
-#define IGNORE_MOD_TAP_INTERRUPT
-#define PERMISSIVE_HOLD
+// #define IGNORE_MOD_TAP_INTERRUPT
+// #define PERMISSIVE_HOLD
 // #define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
 
 #define ONESHOT_TAP_TOGGLE 2
