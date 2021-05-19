52dff5d0207f5015478b85e62c668f6314b39383
Evan Callicoat <apsu@propter.net>
2021-05-19 09:07:16 -0500

    Split shift and space, add layer toggles

--- config.h
+++ config.h
@@ -4,4 +4,9 @@
 #define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
-#define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
+// #define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
+
+/*
+ * Toggle options
+ */
+#define TAPPING_TOGGLE 2
