4709ab34ccb72d320bb46bc690991b4e47d1a2ae
Evan Callicoat <apsu@propter.net>
2021-05-19 08:54:59 -0500

    Try side mods

--- config.h
+++ config.h
@@ -2,9 +2,9 @@
  * Tap options
  */
 #define TAPPING_TERM 200
-// #define IGNORE_MOD_TAP_INTERRUPT
-// #define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
 // #define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
 
-#define ONESHOT_TAP_TOGGLE 2
-#define ONESHOT_TIMEOUT 2000
+// #define ONESHOT_TAP_TOGGLE 2
+// #define ONESHOT_TIMEOUT 2000
