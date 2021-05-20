bd2372c91f13769828b5eadbb3b626ae71025619
Evan Callicoat <apsu@propter.net>
2021-05-19 22:24:50 -0500

    Bump timeout

--- config.h
+++ config.h
@@ -1,12 +1,7 @@
 /*
  * Tap options
  */
-#define TAPPING_TERM 200
+#define TAPPING_TERM 250
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
-// #define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
-
-/*
- * Toggle options
- */
-#define TAPPING_TOGGLE 2
+#define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
