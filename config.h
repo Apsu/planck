e10266773122c8d8a6766d40f9c00e49db2c3f20
Evan Callicoat <apsu@propter.net>
2021-05-20 23:49:13 -0500

    TT toggle

--- config.h
+++ config.h
@@ -4,7 +4,8 @@
 #define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
-// #define TAPPING_FORCE_HOLD // Incompatible with oneshot toggle
 
-// #define ONESHOT_TAP_TOGGLE 2
-// #define ONESHOT_TIMEOUT 2000
+/*
+ * Toggle options
+ */
+#define TAPPING_TOGGLE 2
